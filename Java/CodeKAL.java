f12215c3594dc208223ac2dc8c64f6efb68fe44d
Hilmis <56893563+Hilmis@users.noreply.github.com>
2019-10-23 12:35:02 +0300

    Update CodeKAL.java

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -11,3 +11,17 @@ public class CodeKal {
 	}
 
 }
+
+public class CodeKal {
+
+	public static void main(String[] args) {
+
+		// Aşağıdaki cümleleri ingilizceye çeviriniz.(Noktalama işaretlerinin Java için 
+		// özel anlamları var. Çevirirken silmeyiniz.)
+
+		System.out.println("I love CodeKAL!");
+		System.out.println("I am happy for being a part of CodeKAL!");
+
+	}
+
+}
